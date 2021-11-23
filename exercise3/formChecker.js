af69abbbf028d750774a42a0cb2a596fd87b0986
Hayden Myers <hayden.myers@Haydens-MacBook-Air.local>
2021-11-23 11:46:35 -0600

    js file complete

--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -2,13 +2,18 @@ function checker()
 {
     var at = false;
     var period = false;
-    for(var i=0; i<document.getElementById("text1").value.length; i++)
+    var radio = false;
+    if(document.getElementById("username").value.length<1)
     {
-        if(document.getElementsByName("username").value[i] == '@')
+        alert("You must insert a username");
+    }
+    for(var i=0; i<document.getElementById("username").value.length; i++)
+    {
+        if(document.getElementById("username").value.charAt(i) == '@')
         {
             at = true;
         }
-        if(document.getElementsByName("username").value[i] == '.')
+        if(document.getElementById("username").value.charAt(i) == '.')
         {
             period = true;
         }
@@ -16,5 +21,37 @@ function checker()
     if(!(at && period))
     {
         alert("Username must be in the form of an email");
+    }
+    if(document.getElementById("password").value.length<1)
+    {
+        alert("You must insert a password");
+    }
+    if(!(document.getElementById("bcraw").value >= 0) || document.getElementById("bcraw").value == "")
+    {
+        alert("Quantities must be 0 or more");
+    }
+    if(!(document.getElementById("kobe").value >= 0) || document.getElementById("kobe").value == "")
+    {
+        alert("Quantities must be 0 or more");
+    }
+    if(!(document.getElementById("ramsey").value >= 0) || document.getElementById("ramsey").value == "")
+    {
+        alert("Quantities must be 0 or more");
+    }
+    if(document.getElementById("option1").checked)
+    {
+        radio = true;
+    }
+    else if(document.getElementById("option2").checked)
+    {
+        radio = true;
+    }
+    else if(document.getElementById("option3").checked)
+    {
+        radio = true;
+    }
+    if(!radio)
+    {
+        alert("You must pick a shipping option");
     }
 }
